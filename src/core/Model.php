4a0018e78970361920a1fab8247a670a4777ec4a
Auresorhia <aurelielao456@gmail.com>
2026-05-28 01:36:47 +0200

    Ajout du fichier Model.php pour accéder à la bdd en une fois

---
+++ src/core/Model.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Core;
+
+// Si ton fichier config/database.php n'utilise pas de namespace, 
+// on pourra appeler la classe avec un antislash (\Database) pour indiquer à PHP qu'elle est globale.
+abstract class Model {
+    // Cette propriété sera partagée avec tous les sous-modèles enfants
+    protected $db;
+
+    public function __construct() {
+        // On récupère l'unique instance de PDO via ton Singleton
+        // Le \ devant Database permet de la trouver même si on est dans le namespace App\Core
+        $this->db = \Database::getInstance();
+    }
+}
