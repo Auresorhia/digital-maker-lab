3301e12658748c47ed82fa16aa137577ddc120c6
ERWANCEN <erwance@hotmail.fr>
2026-06-13 23:36:12 +0200

    feat(cms): ajout commentaires sur les methodes du model job #6

--- src/Models/Job/JobModel.php
+++ src/Models/Job/JobModel.php
@@ -7,11 +7,22 @@ class JobModel
 {
     private PDO $db;
 
+    /**
+     * Connexion à la base de données.
+     *
+     * @param PDO $db
+     */
     public function __construct(PDO $db)
     {
         $this->db = $db;
     }
 
+    /**
+     * Récupération de la totalité des métiers.
+     * Rangés par date de création de manière décroissante.
+     *
+     * @return array
+     */
     public function findAll(): array
     {
         $sql = "SELECT id_job, job_name, specialty_id FROM job ORDER BY created_at DESC";
@@ -20,6 +31,12 @@ class JobModel
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    /**
+     * Récupérer un métier en fonction de son id.
+     *
+     * @param integer $id
+     * @return array|null
+     */
     public function findById(int $id): ?array
     {
         $sql = "SELECT id_job, job_name, specialty_id FROM job WHERE id_job = :id";
@@ -30,6 +47,12 @@ class JobModel
         return $result ?: null;
     }
 
+    /**
+     * Récupérer un métier en fonction de sa spécialité.
+     *
+     * @param integer $specialtyId
+     * @return array
+     */
     public function findBySpecialtyId(int $specialtyId): array
     {
         $sql = "SELECT id_job, job_name, specialty_id FROM job WHERE specialty_id = :specialty_id ORDER BY created_at DESC";
@@ -39,6 +62,12 @@ class JobModel
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    /**
+     * Créer un nouveau métier (une nouvelle ligne) dans la base de données.
+     *
+     * @param array $data
+     * @return boolean
+     */
     public function create(array $data): bool
     {
         $sql = "INSERT INTO job (job_name, specialty_id) 
@@ -52,6 +81,13 @@ class JobModel
         ]);
     }
 
+    /**
+     * Mettre à jour les données d'un métier en base de données.
+     *
+     * @param integer $id
+     * @param array $data
+     * @return boolean
+     */
     public function update(int $id, array $data): bool
     {
         $sql = "UPDATE job 
@@ -69,6 +105,12 @@ class JobModel
         ]);
     }
 
+    /**
+     * Supprimer un métier en fonction de son id.
+     *
+     * @param integer $id
+     * @return boolean
+     */
     public function delete(int $id): bool
     {
         $sql = "DELETE FROM job WHERE id_job = :id";
