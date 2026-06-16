6c5713b0226abf9f0666e086729cbd83d6bdd1e6
KCastanho <kevincastanho.pro@gmail.com>
2026-06-16 15:59:25 +0200

    feat(searchbar): ajout du composant recherche et de la route API, #18 #60

---
+++ src/Views/front/searchbar/searchbar.php
@@ -0,0 +1,20 @@
+<div class="search-wrapper">
+    <div class="search-container">
+        <span class="search-icon">&#128269;</span>
+        <input
+            type="text"
+            id="search-input"
+            class="search-input"
+            placeholder="Rechercher..."
+            autocomplete="off"
+            aria-label="Rechercher un métier"
+            aria-autocomplete="list"
+            aria-controls="search-dropdown"
+        />
+    </div>
+    <ul id="search-dropdown" class="search-dropdown search-dropdown--hidden" role="listbox"></ul>
+</div>
+
+<link rel="stylesheet" href="/assets/css/searchbar.css">
+
+<script src="/assets/js/search.js"></script>
