d43d36d148ab64055bfbe457dc4d6dc5bdbb0802
KCastanho <kevincastanho.pro@gmail.com>
2026-06-21 17:56:29 +0200

    modification z index suite à un changement de Aurélie

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_HOST',     '127.0.0.1');
+define('DB_HOST',     'ukpnwnykevincast.mysql.db');
 define('DB_PORT',     '3306');
-define('DB_NAME',     'digital_maker_lab');
-define('DB_USER',     'root');
-define('DB_PASSWORD', '');
-define('DB_CHARSET',  'utf8');
+define('DB_NAME',     'ukpnwnykevincast');
+define('DB_USER',     'ukpnwnykevincast');
+define('DB_PASSWORD', 'ZPFSBnAb1');
+define('DB_CHARSET',  'utf8'); 
